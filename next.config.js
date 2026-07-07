22a86b512fb8dfae7a1acc2f7f659aa86ad14b8a
juanjsales <juangomes.sales@gmail.com>
2026-07-07 02:56:51 -0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -9,16 +9,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(mp4|webm)$/i,
-      type: 'asset/resource',
-      generator: {
-        filename: 'static/media/[name].[hash][ext]',
-      },
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
